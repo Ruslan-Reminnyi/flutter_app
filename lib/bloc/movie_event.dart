30d5716c7f30ba73d730608ff6c96668499efdc4
Ruslan Reminnyi <r.reminnyi@mobidev.biz>
2021-05-25 19:22:08 +0300

    removing additional code, creating constants in order to avoid magic numbers

--- lib/bloc/movie_event.dart
+++
@@ -1,9 +0,0 @@
-abstract class MovieEvent {
-  const MovieEvent();
-}
-
-class LoadedTrendingPageEvent extends MovieEvent {}
-
-class LoadedUpcomingPageEvent extends MovieEvent {}
-
-class LoadedPhotoPageEvent extends MovieEvent {}
